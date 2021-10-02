0eb024392156fa0acef857ce9a9f296a87095e28
Malcolm Sinclair <sinclair.m.x@gmail.com>
2021-10-02 17:53:11 -0400

    add jump to char on click

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var char = document.getElementById('char');
 var obstacle = document.getElementById('obstacle');
 
 // Create function for jump
-function jump() {
-    char.classList.add('charJump');
+function jump(){
+    char.classList.add("charJump");
 }
 
