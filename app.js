30a74ef22c7166381e2bd7d23139c3edfddad2b1
Malcolm Sinclair <sinclair.m.x@gmail.com>
2021-10-02 18:03:02 -0400

    modify jump function

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var obstacle = document.getElementById('obstacle');
 
 // Create function for jump
 function jump(){
-    char.classList.add("charJump");
+    if (char.classList != 'charJump') {
+        char.classList.add("charJump");
+    }
+    setTimeout(function(){
+        char.classList.remove('charJump');
+    }, 500)
 }
 
