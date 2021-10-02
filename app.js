71dfa0e6bc7849e928b455ac2e27a0e1d842a264
Malcolm Sinclair <sinclair.m.x@gmail.com>
2021-10-02 17:42:20 -0400

    create function to add jump animation to char

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+// Create variables to access char and obstacle
 var char = document.getElementById('char');
 var obstacle = document.getElementById('obstacle');
+
+// Create function for jump
+function jump() {
+    char.classList.add('charJump');
+}
+
